f6a5f8ce7aa2050822eb5f003b2c9563b18e1367
Joseph Surla <josephsurla@gmail.com>
2021-05-06 13:37:13 +0900

    Add api createTodo

--- pages/api/getTodos.js
+++ pages/api/getTodos.js
@@ -1,26 +1,14 @@
-const Airtable = require('airtable');
-const base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
-
-const table = base(process.env.AIRTABLE_TABLE_NAME);
-
-const getMinifiedRecord = (record) => {
-    if (!record.fields.completed) {
-        record.fields.completed = false;
-    }
-    return {
-        id: record.id,
-        fields: record.fields
-    }
-}
-
-const minifyRecords = (records) => {
-    return records.map(record => getMinifiedRecord(record));
-}
+import {table, minifyRecords} from "./utils/Airtable.js"
 
 export default async (req, res) => {
-    const records = await table.select({}).firstPage();
-    const minifiedRecords = minifyRecords(records);
-    
-    res.statusCode = 200;
-    res.json(minifiedRecords);
-}
+    try {
+        const records = await table.select({}).firstPage();
+        const minifiedRecords = minifyRecords(records);
+        
+        res.statusCode = 200;
+        res.json(minifiedRecords);
+    } catch(err) {
+        res.statusCode = 500;
+        res.json({msg: 'Something went wrong'});
+    }
+};
