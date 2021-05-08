82ee30126ddf4eb9a8532b8c4f77cea95f47cecd
Joseph Surla <josephsurla@gmail.com>
2021-05-08 15:19:34 +0900

    Fetch data using getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import Navbar from '../components/Navbar'
+import Head from 'next/head';
+import Navbar from '../components/Navbar';
+import { table, minifyRecords } from './api/utils/airtable';
 
-export default function Home() {
+export default function Home({ initialTodos }) {
+  console.log(initialTodos);
   return (
     <div>
       <Head>
@@ -14,4 +16,22 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const todos = await table.select({}).firstPage();
+    return {
+      props: {
+        initialTodos: minifyRecords(todos)
+      }
+    } 
+  } catch(err) {
+      console.error(err);
+      return {
+        props: {
+          err: "Something went wrong"
+        }
+      }
+  }
 }
